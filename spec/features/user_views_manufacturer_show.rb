59c4a044607ee47ac3e996879a7dfcd1a3c88f68
Patrick <pa@arthurfamily.ws>
2014-07-01 11:51:27 -0400

    Final

---
+++ spec/features/user_views_manufacturer_show.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+feature 'user visits individual manufacturer page' do
+
+  scenario "user sees manufacturer information" do
+    manufacturer = FactoryGirl.create(:manufacturer)
+
+    visit "/manufacturers/#{manufacturer.id}"
+
+    expect(page).to have_content "GM"
+    expect(page).to have_content "US"
+  end
+end
---
+++ spec/features/user_views_show_car_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+feature 'user visits individual car page' do
+
+  scenario "user sees information on car" do
+
+    car = FactoryGirl.create(:car)
+
+    visit "/cars/#{car.id}"
+
+    expect(page).to have_content "GM"
+    expect(page).to have_content "Blue"
+    expect(page).to have_content 1950
+    expect(page).to have_content 100,000
+
+  end
+end
